5f4298ca192e5b3c8e86426ff52535a1ca008604
Toon Ketels <toon@puresign.be>
2012-05-27 14:59:43 +0200

    Created different application demonstration switch behavior of controller.

--- src/app.php
+++ src/app.php
@@ -1,14 +1,30 @@
 <?php
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
+function is_leap_year($year = null) {
+  if($year === null) {
+    $year = date('Y');
+  }
+
+  return 0 == $year % 400 || (0 == $year % 4 && 0 != $year % 100);
+}
+
+
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array(
-  'name' => 'Wold',
-  '_controller' => 'render_template'
+
+$routes->add('leap_year', new Routing\Route('/is-leap-year/{year}', array(
+  'year' => null,
+  '_controller' => function($request) {
+    // Defending of it beging a leap year, we return different output.
+    if(is_leap_year($request->attributes->get('year'))) {
+      return new Response("Yes, this is a leap year.");
+    }
+
+    return new Response("No, no leap year.");
+  },
 )));
-$routes->add('bye', new Routing\Route('bye', array(
-  '_controller' => 'render_template'
-)));
+
 
 return $routes;
