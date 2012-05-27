83c7c1a1da2dc9545d8dd29a561a5f21b7c4a06b
Toon Ketels <toon@puresign.be>
2012-05-27 02:16:47 +0200

    Make use of HttpFoundation.

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
 <?php
 
-$input = isset($_GET['name']) ? $_GET['name'] : "World";
+require_once __DIR__.'/autoload.php';
 
-header("Content-Type: text/html; charset=utf-8");
+/**
+ * HttpFoundation: Object oriented wrapper around request/response.
+ */
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
-printf("Hello %s", htmlspecialchars($input, ENT_QUOTES, 'UTF-8'));
+$request = Request::createFromGlobals();
+
+$input = $request->get('name', 'World');
+
+$response = new Response(sprintf('Hello %s', htmlspecialchars($input, ENT_QUOTES, 'UTF-8')));
+
+$response->send();
+
