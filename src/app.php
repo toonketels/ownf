2ece6cfc61ccc5f0cb1697ff404bf4e57543b441
Toon Ketels <toon@puresign.be>
2012-05-27 14:45:28 +0200

    Added controller.

--- src/app.php
+++ src/app.php
@@ -3,7 +3,12 @@
 use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
-$routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'Wold')));
-$routes->add('bye', new Routing\Route('bye'));
+$routes->add('hello', new Routing\Route('/hello/{name}', array(
+  'name' => 'Wold',
+  '_controller' => 'render_template'
+)));
+$routes->add('bye', new Routing\Route('bye', array(
+  '_controller' => 'render_template'
+)));
 
 return $routes;
--- web/index.php
+++ web/index.php
@@ -8,6 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 // Only "use" Routing so we can grab everythign inside it
 use Symfony\Component\Routing;
 
+/**
+ * This is the default contoller. It extracts the
+ * routes from the request and renderds the template
+ * (pass a templates content to response object).
+ */
+function render_template($request)
+{
+  extract($request->attributes->all(), EXTR_SKIP);
+  ob_start();
+  include sprintf( __DIR__.'/../src/pages/%s.php', $_route);
+
+  return new Response(ob_get_clean());
+}
+
+
 $request = Request::createFromGlobals();
 // Move the routes to own file. This is application specific
 // logic and should be seperated from this lib general file.
@@ -21,12 +36,12 @@ $matcher = new Routing\Matcher\UrlMatcher($routes, $context);
 // Since requesting a url which is not in the routeCollection
 // will throw an error: Catch that exeption and respond with 404.
 try {
-  // Will return the parts of the current path: first item is always _route.
-  extract($matcher->match($request->getPathInfo()), EXTR_SKIP);
-  ob_start();
-  include sprintf(__DIR__.'/../src/pages/%s.php', $_route);
-
-  $response = new Response(ob_get_clean());
+  $request->attributes->add($matcher->match($request->getPathInfo()));
+  // To get the response content: ask the route to execute it's _controller
+  // callback function. Each route should hafe this _controller key.
+  // A controller can be a custom callback function or the default
+  // render_template above.
+  $response = call_user_func($request->attributes->get('_controller'), $request);
 } catch (Routing\Exception\ResourceNotFoundException $e) {
   $response = new Response('Not Found', 404);
 } catch (Exception $e) {
