af1ec4c23d7c4abbde12304368ca538a2d7bd396
Toon Ketels <toon@puresign.be>
2012-05-27 13:47:53 +0200

    Make use of url generation via Routing.
    End of part 4.

--- src/app.php
+++ src/app.php
@@ -1,6 +1,6 @@
 <?php
 
-Use Symfony\Component\Routing;
+use Symfony\Component\Routing;
 
 $routes = new Routing\RouteCollection();
 $routes->add('hello', new Routing\Route('/hello/{name}', array('name' => 'Wold')));
--- src/pages/bye.php
+++ src/pages/bye.php
@@ -1,1 +1,9 @@
-Goodbye sir.
+<p>Goodbye sir.</p>
+
+<?php
+  use Symfony\Component\Routing;
+  $generator = new Routing\Generator\UrlGenerator($routes, $context);
+  $link =  $generator->generate('hello', array('name' => 'Toon'), TRUE);
+?>
+
+<p><a href="<?php echo $link; ?>">Say hi to Toon</a></p>
